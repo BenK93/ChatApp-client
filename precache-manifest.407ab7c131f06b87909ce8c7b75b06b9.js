db93670e8ee4d028545e957f62199844022c7bd4
Oded Nir <odednir@icloud.com>
2020-09-29 17:25:31 +0300

    Updates

---
+++ precache-manifest.407ab7c131f06b87909ce8c7b75b06b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d787a5a7009ed2da06b5482030066d3",
+    "url": "/ChatApp-client/index.html"
+  },
+  {
+    "revision": "3126ce95ed2aea7d2ad2",
+    "url": "/ChatApp-client/static/css/main.9e2fe869.chunk.css"
+  },
+  {
+    "revision": "3af804d15e32cdd59e38",
+    "url": "/ChatApp-client/static/js/2.825ea0d6.chunk.js"
+  },
+  {
+    "revision": "555f72d435c87689937422779dcac070",
+    "url": "/ChatApp-client/static/js/2.825ea0d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3126ce95ed2aea7d2ad2",
+    "url": "/ChatApp-client/static/js/main.ba682b5b.chunk.js"
+  },
+  {
+    "revision": "f4d03ea34386b8a979d6",
+    "url": "/ChatApp-client/static/js/runtime-main.b77dc1cb.js"
+  }
+]);
